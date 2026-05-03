c530829fa0aaa651ebdf10f8bc1aaa3059db8c2b
Aräjtav <arajtav@arajtav.com>
2026-05-03 21:34:42 +0200

    feat: add mock cli

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+use std::path::Path;
+
+use clap::{Parser, ValueEnum};
+
+#[derive(Debug, Parser)]
+#[command(arg_required_else_help = true)]
+struct Cli {
+    /// Output file location, if not present the output will be printed to stdout.
+    #[arg(short = 'o', long)]
+    out: Option<Box<Path>>,
+
+    /// Regex kind, different regex engines support different features and use different syntax.
+    #[arg(long, value_enum, default_value_t = RegexKind::PosixEre)]
+    kind: RegexKind,
+
+    /// Input file.
+    input: Box<Path>,
+}
+
+#[derive(Debug, ValueEnum, Clone, Copy, PartialEq, Eq)]
+#[clap(rename_all = "snake_case")]
+enum RegexKind {
+    PosixEre,
+    Pcre,
+    Ecmascript,
+    Python,
+    Java,
+    Re2,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+    dbg!(cli);
 }
