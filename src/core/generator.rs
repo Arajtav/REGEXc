fe36c0891f0cce42498a3f119bf43c9166266cf6
Aräjtav <arajtav@arajtav.com>
2026-05-12 18:44:39 +0200

    feat: remove redundant non capturing groups

--- src/core/generator.rs
+++ src/core/generator.rs
@@ -1,45 +1,78 @@
+use std::borrow::Cow;
+
 use crate::{
     RegexKind,
     core::{lexer::Builtin, processor::InlinedExpression},
 };
 
-fn generate_re2(expr: InlinedExpression) -> Result<String, String> {
-    match expr {
-        InlinedExpression::Optional(inner) => Ok(format!("(?:{})?", generate_re2(*inner)?)),
-        InlinedExpression::Multiple(inner) => Ok(format!("(?:{})+", generate_re2(*inner)?)),
-        InlinedExpression::Some(inner) => Ok(format!("(?:{})*", generate_re2(*inner)?)),
-        InlinedExpression::Literal(literal) => Ok(escape_whitespace(&regex::escape(&literal))),
-        InlinedExpression::Joined(v) => v.into_iter().map(generate_re2).collect(),
-        InlinedExpression::Alternative(v) => Ok(format!(
-            "(?:{})",
-            v.into_iter()
-                .map(generate_re2)
-                .collect::<Result<Vec<_>, _>>()?
-                .join("|")
-        )),
-        InlinedExpression::Builtin(b) => Ok(match b {
-            Builtin::Digit => String::from("\\d"),
-            Builtin::WordChar => String::from("\\w"),
-            Builtin::WhiteSpace => String::from("\\s"),
-            Builtin::Tab => String::from("\\t"),
-            Builtin::CarriageReturn => String::from("\\r"),
-            Builtin::Linefeed => String::from("\\n"),
-            Builtin::VerticalTab => String::from("\\v"),
-            Builtin::FormFeed => String::from("\\f"),
-            Builtin::Nul => String::from("\\0"),
-            Builtin::Space => String::from(" "),
-        }),
-        InlinedExpression::Char(c) => Ok(String::from(c)),
-        InlinedExpression::Nothing => Ok(String::new()),
+fn make_atom(s: Cow<'static, str>, a: bool) -> String {
+    if a {
+        s.into_owned()
+    } else {
+        format!("(?:{s})")
+    }
+}
+
+impl InlinedExpression {
+    pub fn build_re2(self) -> (Cow<'static, str>, bool) {
+        match self {
+            InlinedExpression::Nothing => (Cow::Borrowed(""), true),
+            InlinedExpression::Literal(literal) => (
+                Cow::Owned(escape_whitespace(&regex::escape(&literal))),
+                false,
+            ),
+            InlinedExpression::Char(c) => (Cow::Owned(String::from(c)), true),
+            InlinedExpression::Optional(inner) => {
+                let (inner, atom) = inner.build_re2();
+                (Cow::Owned(make_atom(inner, atom) + "?"), true)
+            }
+            InlinedExpression::Multiple(inner) => {
+                let (inner, atom) = inner.build_re2();
+                (Cow::Owned(make_atom(inner, atom) + "+"), true)
+            }
+            InlinedExpression::Some(inner) => {
+                let (inner, atom) = inner.build_re2();
+                (Cow::Owned(make_atom(inner, atom) + "*"), true)
+            }
+            InlinedExpression::Joined(v) => (
+                Cow::Owned(v.into_iter().map(|a| a.build_re2().0).collect()),
+                false,
+            ),
+            InlinedExpression::Alternative(v) => (
+                Cow::Owned(format!(
+                    "(?:{})",
+                    v.into_iter()
+                        .map(|a| a.build_re2().0)
+                        .collect::<Vec<_>>()
+                        .join("|")
+                )),
+                false,
+            ),
+            InlinedExpression::Builtin(b) => (
+                Cow::Borrowed(match b {
+                    Builtin::Digit => "\\d",
+                    Builtin::WordChar => "\\w",
+                    Builtin::WhiteSpace => "\\s",
+                    Builtin::Tab => "\\t",
+                    Builtin::CarriageReturn => "\\r",
+                    Builtin::Linefeed => "\\n",
+                    Builtin::VerticalTab => "\\v",
+                    Builtin::FormFeed => "\\f",
+                    Builtin::Nul => "\\0",
+                    Builtin::Space => " ",
+                }),
+                true,
+            ),
+        }
     }
 }
 
 pub fn generate(expr: InlinedExpression, regex_kind: RegexKind) -> Result<String, String> {
-    if regex_kind != RegexKind::Re2 {
-        todo!("only re2 is supported as of now")
+    if regex_kind == RegexKind::Re2 {
+        Ok(expr.build_re2().0.into_owned())
+    } else {
+        Err(String::from("Only RE2 is supported as of now"))
     }
-
-    generate_re2(expr)
 }
 
 fn escape_whitespace(text: &str) -> String {
