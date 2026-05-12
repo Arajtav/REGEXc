940cc38002456807fdfbe2ff3569f396eb116479
Aräjtav <arajtav@arajtav.com>
2026-05-12 18:57:29 +0200

    test: fix tab escape test (it did not actually use tab)

--- src/core.rs
+++ src/core.rs
@@ -32,7 +32,7 @@ mod tests {
 
     re2_test!(lit, r#"EXPORT := "abc""#, r"abc");
     re2_test!(lit_escape_newline, r#"EXPORT := "\n""#, r"\n");
-    re2_test!(lit_escape_tab, r#"EXPORT := "    ""#, r"\t");
+    re2_test!(lit_escape_tab, "EXPORT := \"\t\"", r"\t");
     re2_test!(lit_escape_dot, r#"EXPORT := ".""#, r"\.");
     re2_test!(lit_join, r#"EXPORT := "a" + DIGIT"#, r"a\d");
     re2_test!(alt_chars_chain, r#"EXPORT := "a" / "b" / DIGIT"#, r"[ab\d]");
